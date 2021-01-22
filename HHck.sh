2292d3eb8a00e5bfee2f2c9e980439effc0b5977
HansHax <77719843+HansHax@users.noreply.github.com>
2021-01-22 07:01:47 +0700

    Add files via upload

---
+++ HHck.sh
@@ -0,0 +1,149 @@
+#!usr/bin/bash
+clear
+bi='\33[34;1m' #biru
+me='\33[31;1m' #merah
+pu='\33[37;1m' #putih
+lg='\33[32;1m' #lightgreen
+#jangan diubah kalau tidak tau
+sleep 3
+echo $lg"[+]--------------------------------------[+]"
+echo ""
+echo ""
+echo $me"▀   ▀  ▀   ▀"
+echo $me"▀   ▀  ▀   ▀   ▀▀▀▀  ▀"
+echo $me"▀▀▀▀▀  ▀▀▀▀▀  ▀      ▀  ▀"
+echo $pu"▀   ▀  ▀   ▀  ▀      ▀ ▀"
+echo $pu"▀   ▀  ▀   ▀   ▀▀▀▀  ▀  ▀    v0.1"
+echo ""
+echo $pu"~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $pu"Author   : HanHaX"
+echo $pu"Contact  : +6287850013150"
+echo $pu"GitHub   : HanHax"
+echo $pu"~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $pu""
+echo $pu""
+echo $lg"[+]--------------------------------------[+]"
+echo ""
+echo ""
+echo $bi"Silahkan Pilih Menu Dibawah Ini:"
+#mulai dari sini
+sleep 2
+     echo ""
+     echo ""
+     echo $bi"1.] Hack Kamera HP"
+     echo ""
+     echo ""
+     echo $bi"2.] Sadap WA"
+     echo ""
+     echo ""
+sleep 2
+   read -p "Pilih Nomor:" pilih
+if [ $pilih = "1" ]
+then
+   read -p "Masukkan Nomor Target:" nomor
+      sleep 2
+clear
+       echo "Membaca pilihan"
+      sleep 3
+     echo "Sedang Menanam Virus Ke" $nomor
+      sleep 3
+   echo $lg"Loading"
+      sleep 4
+      echo $lg"[+]                               [+] 0%"
+sleep 8
+      echo $lg"[+]=                              [+] 3%"
+sleep 8
+      echo $lg"[+]====                           [+] 10%"
+sleep 8
+      echo $lg"Mendapatkan IP Target"
+sleep 5
+      echo $lg"[+]=======                        [+] 20%"
+sleep 8
+      echo $lg"[+]==========                     [+] 25%"
+sleep 10
+      echo $lg"[+]===============                [+] 50%"
+sleep 14
+      echo $lg"[+]=====================          [+] 75%"
+sleep 8
+      echo $lg"[+]==========================     [+] 80%"
+sleep 10
+      echo $lg"[+]=============================  [+] 95%"
+sleep 5
+      echo $lg"Sedang Mengambil Gambar Dari Kamera Hp" $nomor
+sleep 5
+      echo $lg"Memuat Link Gambar"
+sleep 5
+      echo $lg"Berhasil Memuat Link Gambar, Silahkan Copy Lalu Paste Di Chrome"
+sleep 2
+clear
+sleep 2
+echo $bi"Link Gambar: https://www.mediafire.com/file/si5cfssk8up6aty/gambar_hasil_hack_kamera.jpg/file"
+sleep 2
+     echo $lg"Terimakasih Telah Menggunakan Tools ini"
+sleep 1
+     echo $pu"jika ingin menggunakan tools ini lagi, silahkan ketik $sh HHck.sh"
+fi
+
+#ini batas nomor 2
+if [ $pilih = "2" ]
+then                                                                                                                                             sleep 1
+           read -p "Masukan Nomor Target:" nomor
+sleep 2
+      clear
+           echo "Sedang Membaca Pilihan"
+sleep 2
+           echo "Masuk Ke Perangkat" $nomor
+sleep 3
+clear
+   echo $lg"Loading"
+      sleep 4
+      echo $lg"[+]                               [+] 0%"
+sleep 8
+      echo $lg"[+]=                              [+] 3%"
+sleep 8
+      echo $lg"[+]====                           [+] 10%"
+sleep 8
+      echo $lg"Sedang Mencari File" $nomor
+sleep 5
+      echo $lg"[+]=======                        [+] 20%"
+sleep 8
+      echo $lg"[+]==========                     [+] 25%"
+sleep 10
+      echo $lg"[+]===============                [+] 50%"
+sleep 14
+      echo $lg"[+]=====================          [+] 75%"
+sleep 8
+      echo $lg"[+]==========================     [+] 80%"
+sleep 10
+      echo $lg"[+]=============================  [+] 95%"
+sleep 2
+      echo "Berhasil Masuk Ke Perangkat"
+sleep 3
+      echo "Berhasil Mencari file yang dituju"
+sleep 3
+      echo "membaca isi file lalu menerjemahkan, mungkin ini akan lama"
+sleep 45
+     echo "Selesai Menerjemahkan"
+sleep 2
+clear
+              echo $bi"[2020-04-03 21.35] Mat Malam Sayang"
+sleep 1
+              echo $bi"[2020-04-03 21.36] Malam Juga Sayang"
+sleep 1
+              echo $bi"[2020-04-04 06.12] Selamat Pagi Sayangku, Ayo Bangun, Cepetan Mandi Gih"
+sleep 1
+              echo $bi"[2020-04-04 09.10] Sayang, Nanti Kita jalan kan?"
+sleep 1
+              echo $bi"[2020-04-04 09.10] Pagi Juga sayangkuuuu, Jadi Jalan Dong"
+sleep 1
+              echo $bi"[2020-04-04 23.36] Sayang Kamu Belum Tidur? Cepet Tidur Nanti Kamu Sakit lho"
+sleep 1
+              echo $bi"[2020-04-04 23.36] Iya Sayang Aku Ini Siap Siap Mau Tidur, Love You"
+sleep 1
+              echo $bi"[2020-04-04 23.37] Love You To Sayang Muaah"
+sleep 3
+              echo $pu"Sambungan Terputus..."
+sleep 1                                                                                                                                                    echo $pu"memulai Ulang"
+sleep 2
+cd $HOME
+fi
